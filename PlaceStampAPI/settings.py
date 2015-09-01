7126301017cdf417cd8e6be90bb7f249f167e384
Giacomo <giacomoalbe@gmail.com>
2015-09-01 16:43:09 +0200

    Aggiunto STATIC_ROOT

--- PlaceStampAPI/settings.py
+++ PlaceStampAPI/settings.py
@@ -82,7 +82,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-# STATIC_ROOT = 'img/'
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 #STATICFILES_DIR = (os.path.join(BASE_DIR, 'static/'),)
 
